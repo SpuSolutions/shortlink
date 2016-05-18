c4af90c595d1fbed26694e2702b333c6460c4205
Salvatore Bellomo <bellomosalvo@gmail.com>
2016-05-19 00:49:29 +0200

    first encryption Class stub

--- app/src/Security/Encryption.php
+++ app/src/Security/Encryption.php
@@ -9,7 +9,60 @@
 namespace App\Security;
 
 
-class Encryption
-{
+class Encryption{
+
+    private $secretKey;
+    private $method;
+
+    public function __construct(){
+
+    }
+
+    /**
+     * Get the list of cipher methods supported by openssl module
+     * @return array
+     */
+    public function getCipherMethod(){
+        return openssl_get_cipher_methods();
+    }
+
+    public  function safe_b64encode($string) {
+        $data = base64_encode($string);
+        $data = str_replace(array('+','/','='),array('-','_',''),$data);
+        return $data;
+    }
+    public function safe_b64decode($string) {
+        $data = str_replace(array('-','_'),array('+','/'),$string);
+        $mod4 = strlen($data) % 4;
+        if ($mod4) {
+            $data .= substr('====', $mod4);
+        }
+        return base64_decode($data);
+    }
+
+    public function encode($value){
+        if(!$value){return false;}
+        $text = $value;
+        //temporary iv
+        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+
+        $encrypted = openssl_encrypt($text, $this->method, $this->secretKey, $iv);
+        return trim($this->safe_b64encode($encrypted));
+
+    }
+
+    public function decode($value){
+        if(!$value){return false;}
+        $encrypted = $this->safe_b64decode($value);
+        //temporary iv
+        $iv_size = mcrypt_get_iv_size(MCRYPT_RIJNDAEL_256, MCRYPT_MODE_ECB);
+        $iv = mcrypt_create_iv($iv_size, MCRYPT_RAND);
+
+        $text = openssl_decrypt($encrypted, $this->method, $this->secretKey, $iv);
+        return trim($this->safe_b64encode($text));
+
+    }
+
 
 }
