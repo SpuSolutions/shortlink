7a5d56d1c0d2f9c438d3a066a818bf1182fc41e4
Salvatore Bellomo <bellomosalvo@gmail.com>
2016-05-19 09:42:19 +0200

    added Test and functions in Encryption class

---
+++ app/tests/EncryptionTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * Created by PhpStorm.
+ * User: salvob
+ * Date: 19/05/2016
+ * Time: 00:53
+ */
+class EncryptionTest extends PHPUnit_Framework_TestCase
+{
+    /*
+Sample Call:
+$str = "myPassword";
+$converter = new Encryption;
+$encoded = $converter->encode($str );
+$decoded = $converter->decode($encode);
+echo "$encoded<p>$decoded";
+*/
+
+}
