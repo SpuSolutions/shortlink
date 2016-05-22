59984fa536a1dd69eea56e0218db428d5c35817c
Salvatore Bellomo <bellomosalvo@gmail.com>
2016-05-22 23:48:03 +0200

    add some PHPdoc

--- app/src/Link/Link.php
+++ app/src/Link/Link.php
@@ -11,67 +11,106 @@ Class Link {
 	private $created;
 	private $errors;
 
+	/**
+	 * Link constructor.
+     */
 	public function __construct()
 	{
 		$this->created = time();
 		$this->errors = array();
 	}
 
+	/**
+	 * @param $word
+     */
 	public function setWord($word)
 	{
 		$this->word = $word;
 	}
 
+	/**
+	 * @return mixed
+     */
 	public function getWord()
 	{
 		return $this->word;
 	}
 
+	/**
+	 * @param $url
+     */
 	public function setUrl($url)
 	{
 		$this->url = $url;
 	}
 
+	/**
+	 * @return mixed
+     */
 	public function getUrl()
 	{
 		return $this->url;
 	}
 
+	/**
+	 * @param $expireTime
+     */
 	public function setExpireTime($expireTime)
 	{
 		$this->expireTime = $expireTime;
 	}
 
+	/**
+	 * @return int
+     */
 	public function getExpireTime()
 	{
 		return $this->expireTime;
 	}
 
+	/**
+	 * @param $created
+     */
 	public function setCreated($created)
 	{
 		$this->created = $created;
 	}
 
+	/**
+	 * @return int
+     */
 	public function getCreated()
 	{
 		return $this->created;
 	}
 
+	/**
+	 * @param $passwordProtected
+     */
 	public function setPasswordProtected($passwordProtected)
 	{
 		$this->passwordProtected = $passwordProtected;
 	}
 
+	/**
+	 * @return bool
+     */
 	public function getPasswordProtected()
 	{
 		return $this->passwordProtected;
 	}
 
+	/**
+	 * @return bool
+     */
 	public function isValid()
 	{
 		return $this->isValidWord() && $this->isValidUrl() && $this->isValidExpireTime() && $this->isValidCreated() && $this->isValidPasswordProtected();
 	}
 
+	/**
+	 * @return string
+     */
 	public function __toString()
 	{
 		$link = [
@@ -84,16 +123,25 @@ Class Link {
 		return json_encode($link);
 	}
 
+	/**
+	 * @return bool
+     */
 	public function hasExpired()
 	{
 		return time() > $this->created + ($this->expireTime * 60);
 	}
 
+	/**
+	 * @return array
+     */
 	public function getErrors()
 	{
 		return $this->errors;
 	}
 
+	/**
+	 * @return float|int
+     */
 	public function getRemainingMinutes()
 	{
 		$remainingMinutes = ceil(($this->created + $this->expireTime * 60 - time()) / 60);
@@ -104,31 +152,49 @@ Class Link {
 		}
 	}
 
+	/**
+	 * @param $message
+     */
 	private function setError($message)
 	{
 		$this->errors[] = $message;
 	}
 
+	/**
+	 * @return bool
+     */
 	private function isValidWord()
 	{
 		return (ctype_alpha($this->word));
 	}
 
+	/**
+	 * @return bool
+     */
 	private function isValidUrl()
 	{
 		return (!filter_var($this->url, FILTER_VALIDATE_URL) === false);
 	}
 
+	/**
+	 * @return bool
+     */
 	private function isValidExpireTime()
 	{
 		return (is_int($this->expireTime) && $this->expireTime >= 1 && $this->expireTime <= 60);
 	}
 
+	/**
+	 * @return bool
+     */
 	private function isValidCreated()
 	{
 		return (is_int($this->created) && strlen($this->created) === 10);
 	}
 
+	/**
+	 * @return bool
+     */
 	private function isValidPasswordProtected()
 	{
 		return in_array($this->passwordProtected, [false, true], true);
