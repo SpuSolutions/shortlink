b6c924a0ad5da164ab6284b10845e5752e9bed73
Salvatore Bellomo <bellomosalvo@gmail.com>
2016-05-23 02:28:53 +0200

    added phpDoc, progress integration with Service 50%

--- app/tests/LinkServiceTest.php
+++ app/tests/LinkServiceTest.php
@@ -5,136 +5,167 @@ use App\Link\LinkDaoInterface;
 use App\Link\LinkService;
 use App\Link\LinkFactory;
 
-class LinkServiceTest extends \PHPUnit_Framework_TestCase {
+class LinkServiceTest extends \PHPUnit_Framework_TestCase
+{
 
-	private $linkDao;
+    private $linkDao;
 
-	public function setUp()
-	{
-		$this->linkFactory = new LinkFactory();
-		$this->linkDao = $this->getMockBuilder('App\Link\LinkDaoInterface')->setConstructorArgs([new App\Link\LinkFactory(), []])->getMock();
-	}
+    public function setUp()
+    {
+        $this->linkFactory = new LinkFactory();
+        $this->linkDao = $this->getMockBuilder('App\Link\LinkDaoInterface')->setConstructorArgs([new App\Link\LinkFactory(), []])->getMock();
+    }
 
-	public function testGettingLinkGivenAnExistingWordReturnsALinkObject()
-	{
-		$word = "myWord";
 
-		$this->linkDao->method("getByWord")->with("myWord")->will($this->returnCallback(
-			function() { 
-				$l = new Link(); 
-				$l->setWord("myWord"); 
-				$l->setUrl("http://www.google.com");
-				return $l; 
-			}
-		));
+    public function testGettingLinkGivenAnExistingWordReturnsALinkObject()
+    {
+        $word = "myWord";
 
-		$linkService = new LinkService($this->linkDao, $this->linkFactory);
-		$link = $linkService->getByWord($word);
-		$this->assertInstanceOf('App\Link\Link', $link);
+        $this->linkDao->method("getByWord")->with("myWord")->will($this->returnCallback(
+            function () {
+                $l = new Link();
+                $l->setWord("myWord");
+                $l->setUrl("http://www.google.com");
+                return $l;
+            }
+        ));
 
-	}
+        $linkService = new LinkService($this->linkDao, $this->linkFactory);
+        $link = $linkService->getByWord($word);
+        $this->assertInstanceOf('App\Link\Link', $link);
 
-	public function testGettingLinkGivenNonExistingWordReturnsFalse()
-	{
-		$word = "myWord";
+    }
 
-		$this->linkDao->method("getByWord")->with("myWord")->willReturn(false);
+    public function testGettingLinkGivenNonExistingWordReturnsFalse()
+    {
+        $word = "myWord";
 
-		$linkService = new LinkService($this->linkDao, $this->linkFactory);
-		$link = $linkService->getByWord($word);
-		$this->assertFalse($link);
+        $this->linkDao->method("getByWord")->with("myWord")->willReturn(false);
 
-	}
+        $linkService = new LinkService($this->linkDao, $this->linkFactory);
+        $link = $linkService->getByWord($word);
+        $this->assertFalse($link);
 
-	public function testGettingLinkGivenNonExistingEmptyWordReturnsFalse()
-	{
-		$word = "";
+    }
 
-		$this->linkDao->method("getByWord")->with("myWord")->willReturn(false);
+    public function testGettingLinkGivenNonExistingEmptyWordReturnsFalse()
+    {
+        $word = "";
 
-		$linkService = new LinkService($this->linkDao, $this->linkFactory);
-		$link = $linkService->getByWord($word);
-		$this->assertFalse($link);
+        $this->linkDao->method("getByWord")->with("myWord")->willReturn(false);
 
-	}
+        $linkService = new LinkService($this->linkDao, $this->linkFactory);
+        $link = $linkService->getByWord($word);
+        $this->assertFalse($link);
 
-	public function testGettingLinkGivenWithoutWordReturnsFalse()
-	{
-		$word = "";
+    }
 
-		$this->linkDao->method("getByWord")->with("myWord")->willReturn(false);
+    public function testGettingLinkGivenWithoutWordReturnsFalse()
+    {
+        $word = "";
 
-		$linkService = new LinkService($this->linkDao, $this->linkFactory);
-		$link = $linkService->getByWord();
-		$this->assertFalse($link);
+        $this->linkDao->method("getByWord")->with("myWord")->willReturn(false);
 
-	}
+        $linkService = new LinkService($this->linkDao, $this->linkFactory);
+        $link = $linkService->getByWord();
+        $this->assertFalse($link);
 
-	public function testCreatingLinkGivenAWordAndDataReturnsALinkObject()
-	{
-		$word = "testWord";
-		$data = (object)['url' => 'http://www.google.com', 'expireTime' => 60];
+    }
 
-		$this->linkDao->method("create")->withAnyParameters()->will($this->returnCallback(
-			function() { 
-				$l = new Link(); 
-				$l->setWord("testWord");
-				$l->setUrl("http://www.google.com");
-				return $l; 
-			}
-		));
+    public function testCreatingLinkWithPasswordGivenAWordAndDataReturnsALinkObject()
+    {
+        $word = "testWord";
+        $password = "testPassword";
+        $data = (object)['url' => 'http://www.google.com', 'expireTime' => 60, 'password' => $password];
 
-		$linkService = new LinkService($this->linkDao, $this->linkFactory);
-		$link = $linkService->create($word, $data);
-		$this->assertInstanceOf('App\Link\Link', $link);
-	}
+        $exptecetd = $this->linkDao->method("create")->withAnyParameters()->will($this->returnCallback(
+            function () {
+                $l = new Link();
+                $l->setWord("testWord");
 
-	public function testCreatingLinkGivenAnExistingWordAndDataReturnsFalse()
-	{
-		$word = "testWord";
-		$data = (object)['url' => 'www', 'expireTime' => 60];
+                $e = new \App\Security\Encryption("testPassword");
+                $l->setPasswordProtected(true);
+                $l->setUrl($e->encrypt("http://www.google.com", "testPassword"));
 
-		$this->linkDao->method("create")->with()->willReturn(false);
 
-		$linkService = new LinkService($this->linkDao, $this->linkFactory);
-		$link = $linkService->create($word, $data);
-		$this->assertFalse($link);
-	}
+                return $l;
+            }
+        ));
 
-	public function testCreatingLinkGivenAnEmptyWordAndEmptyDataReturnsFalse()
-	{
-		$word = "";
-		$data = (object)[];
+        $linkService = new LinkService($this->linkDao, $this->linkFactory);
+        $link = $linkService->create($word, $data);
+        $this->assertInstanceOf('App\Link\Link', $link);
+        $this->assertEquals($exptecetd,$link );
 
-		$this->linkDao->method("create")->with($word, $data)->willReturn(false);
 
-		$linkService = new LinkService($this->linkDao, $this->linkFactory);
-		$link = $linkService->create($word, $data);
-		$this->assertFalse($link);
-	}
 
-	public function testCreatingLinkGivenAWordAndEmptyDataReturnsFalse()
-	{
-		$word = "testWord";
-		$data = (object)[];
+    }
 
-		$this->linkDao->method("create")->with()->willReturn(false);
+    public function testCreatingLinkGivenAWordAndDataReturnsALinkObject()
+    {
+        $word = "testWord";
+        $data = (object)['url' => 'http://www.google.com', 'expireTime' => 60];
 
-		$linkService = new LinkService($this->linkDao, $this->linkFactory);
-		$link = $linkService->create($word, $data);
-		$this->assertFalse($link);
-	}
+        $this->linkDao->method("create")->withAnyParameters()->will($this->returnCallback(
+            function () {
+                $l = new Link();
+                $l->setWord("testWord");
+                $l->setUrl("http://www.google.com");
+                return $l;
+            }
+        ));
 
-	public function testCreatingLinkGivenAWordAndANonObjectDataReturnsFalse()
-	{
-		$word = "testWord";
-		$data = ["url" => "myurl.net"];
+        $linkService = new LinkService($this->linkDao, $this->linkFactory);
+        $link = $linkService->create($word, $data);
+        $this->assertInstanceOf('App\Link\Link', $link);
+    }
 
-		$this->linkDao->method("create")->with()->willReturn(false);
+    public function testCreatingLinkGivenAnExistingWordAndDataReturnsFalse()
+    {
+        $word = "testWord";
+        $data = (object)['url' => 'www', 'expireTime' => 60];
 
-		$linkService = new LinkService($this->linkDao, $this->linkFactory);
-		$link = $linkService->create($word, $data);
-		$this->assertFalse($link);
-	}
+        $this->linkDao->method("create")->with()->willReturn(false);
+
+        $linkService = new LinkService($this->linkDao, $this->linkFactory);
+        $link = $linkService->create($word, $data);
+        $this->assertFalse($link);
+    }
+
+    public function testCreatingLinkGivenAnEmptyWordAndEmptyDataReturnsFalse()
+    {
+        $word = "";
+        $data = (object)[];
+
+        $this->linkDao->method("create")->with($word, $data)->willReturn(false);
+
+        $linkService = new LinkService($this->linkDao, $this->linkFactory);
+        $link = $linkService->create($word, $data);
+        $this->assertFalse($link);
+    }
+
+    public function testCreatingLinkGivenAWordAndEmptyDataReturnsFalse()
+    {
+        $word = "testWord";
+        $data = (object)[];
+
+        $this->linkDao->method("create")->with()->willReturn(false);
+
+        $linkService = new LinkService($this->linkDao, $this->linkFactory);
+        $link = $linkService->create($word, $data);
+        $this->assertFalse($link);
+    }
+
+    public function testCreatingLinkGivenAWordAndANonObjectDataReturnsFalse()
+    {
+        $word = "testWord";
+        $data = ["url" => "myurl.net"];
+
+        $this->linkDao->method("create")->with()->willReturn(false);
+
+        $linkService = new LinkService($this->linkDao, $this->linkFactory);
+        $link = $linkService->create($word, $data);
+        $this->assertFalse($link);
+    }
 
 }
