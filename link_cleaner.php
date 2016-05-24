8473d8fb4019e0fe5bcead2d29fdffaad7f20fb0
enrico <enrico@enrico.enrico>
2016-05-24 18:35:05 +0100

    Added script to clean expired link files

---
+++ link_cleaner.php
@@ -0,0 +1,17 @@
+<?php
+
+$linkDirectory = "uploads";
+$expireTime = 70; // time to wait in minutes before deleting a link file
+
+$dir = new DirectoryIterator($linkDirectory);
+foreach ($dir as $fileinfo) {
+    
+    // Loop through every link file
+    if (!$fileinfo->isDot() && !$fileinfo->isDir() && strpos($fileinfo, ".") === false) {
+        
+        // Check if link file has expired and delete it
+        if($fileinfo->getCTime() + $expireTime*60 < time()){
+            unlink($fileInfo->getPathname());
+        }
+    }
+}
